3a58c9b4837d40611d2cd4fb3a7945f96bcd6278
saleem <saleem@saleem-PC>
2017-03-16 00:18:28 +0530

    added ApplicationContextConfig.class

---
+++ FairDealBackEnd/src/main/java/com/niit/fairdeal/config/ApplicationContextConfig.java
@@ -0,0 +1,78 @@
+package com.niit.fairdeal.config;
+
+import java.util.Properties;
+
+import javax.sql.DataSource;
+
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBuilder;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import com.niit.fairdeal.domain.Category;
+import com.niit.fairdeal.domain.Product;
+import com.niit.fairdeal.domain.Supplier;
+import com.niit.fairdeal.domain.User;
+
+@Configuration
+@ComponentScan("com.niit.fairdeal")
+@EnableTransactionManagement
+public class ApplicationContextConfig {
+	@Bean(name = "dataSource")
+	public DataSource getH2DataSource() {
+
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+
+		dataSource.setUrl("jdbc:h2:tcp://localhost/~/test");
+
+		dataSource.setDriverClassName("org.h2.Driver");
+
+		dataSource.setUsername("sa");
+		dataSource.setPassword("");
+
+		
+		return dataSource;
+	}
+
+	private Properties getHibernateProperties() {
+		Properties properties = new Properties();
+
+		properties.put("hibernate.dialect", "org.hibernate.dialect.H2Dialect");
+		properties.put("hibernate.show_sql", "true");
+		properties.put("hibernate.hbm2ddl.auto", "update");
+		
+		return properties;
+	}
+
+	@Autowired
+	@Bean(name = "sessionFactory")
+	public SessionFactory getSessionFactory(DataSource dataSource) {
+
+		
+		LocalSessionFactoryBuilder sessionBuilder = new LocalSessionFactoryBuilder(dataSource);
+		sessionBuilder.addProperties(getHibernateProperties());
+
+		sessionBuilder.addAnnotatedClasses(Category.class);
+		sessionBuilder.addAnnotatedClass(User.class);
+		sessionBuilder.addAnnotatedClass(Product.class);
+		sessionBuilder.addAnnotatedClass(Supplier.class);
+		
+		//Alternate to above steps, you can add all the domain objects using the following stmts
+		//sessionBuilder.scanPackages("com.niit.fairdeal.domain");
+
+		return sessionBuilder.buildSessionFactory();
+	}
+
+	@Autowired
+	@Bean(name = "transactionManager")
+	public HibernateTransactionManager getTransactionManager(SessionFactory sessionFactory) {
+		
+		HibernateTransactionManager transactionManager = new HibernateTransactionManager(sessionFactory);
+		return transactionManager;
+	}
+}
